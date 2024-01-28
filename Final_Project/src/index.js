1beadbb5530c4222ddac56ab77a77516b0aa09f5
Vivek <vivekkumar120786@gmail.com>
2024-01-28 23:04:42 +0530

    Add Utils

--- Final_Project/src/index.js
+++ Final_Project/src/index.js
@@ -2,10 +2,25 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    // checking for error
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    // starting the server
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo DB connection failed ", error);
+  });
 
 /*
 import express from "express";
---
+++ Final_Project/src/utils/asynHandler.js
@@ -0,0 +1,27 @@
+// const asyncHandler = () =>{}
+// const asyncHandler = (fn) => () =>{}
+// const asyncHandler = (fn) =>{ async () => {} }
+
+// Taking function as an input , Higher order function using try and catch method
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message || "Internal Server Error",
+//     });
+//   }
+// };
+
+// Using promises
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export default asyncHandler;
